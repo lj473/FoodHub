01512d2201f3153ba7bfaf7dc9f1ff22e1072b73
lj473 <lj473@exeter.ac.uk>
2023-03-10 23:44:39 +0000

    Setup SQL connection script and first function: password check

---
+++ dataRetrieveDB.py
@@ -0,0 +1,36 @@
+# Created by 720018271 on 10/03/2023 for dtsfoodhub
+
+import pyodbc
+import socket
+
+def connectToDB(): #Function to connect to database
+    DeviceName = str(socket.gethostname()) #Get device name to access local SQL server
+    DBConnection = pyodbc.connect('Driver={SQL Server};' #Connect to database
+                      f'Server={DeviceName}\SQLEXPRESS;'
+                      'Database=nfhcw2;'
+                      'Trusted_Connection=yes;')
+    global DBLink
+    DBLink = DBConnection.cursor() #Set database link object to use later
+
+def DBTest(): #Used for testing
+    Result = DBLink.execute('SELECT * FROM Contact')
+    
+    return responseToDict(Result)
+
+def responseToDict(response): #Function to connect to database
+    Columns = [Column[0] for Column in response.description] #Get names of columns
+    
+    Output = []
+    for Row in response.fetchall(): #For each row in the table
+        Output.append(dict(zip(Columns, Row))) #Put row details and column names together
+        
+    return Output
+
+#Functionality functions
+
+def checkPassword(UserName,enteredPassword):
+
+    if responseToDict(DBLink.execute(f"SELECT * FROM Users WHERE UserName = '{UserName}'"))[0].get('UserPwd') == enteredPassword: #If entered password equals the one in database
+        return True
+    else:
+        return False
---
+++ dtsfoodhub.py
@@ -0,0 +1,7 @@
+# Created by 720018271 on 10/03/2023 for dtsfoodhub
+
+import dataRetrieveDB
+
+dataRetrieveDB.connectToDB() #Setup connection to database
+
+print(dataRetrieveDB.DBTest()) #Testing only

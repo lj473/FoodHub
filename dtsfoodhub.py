400b212d59aaf3f34b55fc23a7cc4571f8b89ebc
diego-uoe <dd467@exeter.ac.uk>
2023-03-13 20:39:45 +0000

    Instantiating the UI classes and setting up for login to DB
    Created skeleton for the top-level application. Currently not using the SQL Connectivity Layer functions due to problems with my local machine.

--- dtsfoodhub.py
+++ dtsfoodhub.py
@@ -1,13 +1,72 @@
 # Created by 720018271 on 10/03/2023 for dtsfoodhub
+# Updated by 720085401 on 13/03/2023 for dtsfoodhub
 
+from PyQt5.QtWidgets import *
 import dataRetrieveDB
+from loginScr import Ui_loginScr # Import login screen class from Qt
+from homeScr import Ui_homeScr # Import home screen class from Qt
 
-dataRetrieveDB.connectToDB() #Setup connection to database
-
-print(dataRetrieveDB.DBTest()) #Testing only
+# print(dataRetrieveDB.DBTest()) #Testing only
 
 '''
 For login screen:
 - Use dataRetrieveDB.checkUser() first to see if entered UserName exists
 - Then use dataRetrieveDB.checkPassword() to see if password entered matches the one in the DB
 '''
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        # Showing the login screen as soon as the user starts the application
+        self.show_login_screen()
+
+    def show_login_screen(self):
+        self.loginScr = QWidget()
+        self.ui = Ui_loginScr() # From Qt Designer
+        self.ui.setupUi(self.loginScr)
+        self.loginScr.show()
+
+        # dataRetrieveDB.connectToDB() # Connect to the database before user attempts to log in
+
+        # on click for the "confirm" button -> call the login func to authenticate with DB
+        self.ui.btnConfim.clicked.connect(self.login)
+
+    def show_home_screen(self):
+        """
+        Display the home page.
+        Currently the code will fail due to homeScr.py line 397, 
+        (if you temporarily remove that line from homeScr.py the code will work)
+        """
+        # Once we are logged in we can show the Home Screen
+        self.homeScr = QWidget() 
+        self.ui = Ui_homeScr() # From Qt Designer
+        self.ui.setupUi(self.homeScr)
+        self.homeScr.show()
+    
+    def login(self):
+        """
+        User authenticates against the database. Uses the functions from the SQL Connection Layer.
+        """
+
+        # Testing without using the SQL functions first (temporary)
+
+        if self.ui.editboxUsr.text() == 'test' and self.ui.editboxPass.text() == 'test':
+        #if dataRetrieveDB.checkUser(self.ui.editboxUsr.text()) and dataRetrieveDB.checkPassword(self.ui.editboxUsr.text(), self.ui.editboxPass.text()):
+            message = QMessageBox()
+            message.setText("Logged in.")
+            message.exec_()
+            # Once authenticated we hide the login screen and call the show home screen function
+            self.loginScr.hide()
+            self.show_home_screen()
+        else:
+            message = QMessageBox()
+            message.setText("Invalid Login.")
+            message.exec_()
+
+def main():
+    app = QApplication([])
+    window = MainWindow()
+    app.exec()
+
+main()

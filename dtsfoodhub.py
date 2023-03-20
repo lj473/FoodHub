2219b8832146d345038fe13e5c5c66b9f24c046e
diego-uoe <dd467@exeter.ac.uk>
2023-03-20 18:57:20 +0000

    adding comments,docstrings and improving coding standards

--- dtsfoodhub.py
+++ dtsfoodhub.py
@@ -25,15 +25,17 @@ class MainWindow(QMainWindow):
         self.login_ui.editboxUsr.setText('nfhtest') # ONLY FOR TESTING
         self.login_ui.editboxPass.setText('nfhtestpwd') # ONLY FOR TESTING
 
-        # On click for the "confirm" button -> call the login func to authenticate with DB
+        # Call the login function to authenticate using the credentials provided by the user
         self.login_ui.btnConfim.clicked.connect(self.login)
+
+        # Allowing the user to exit the program by clicking the 'Cancel' button
         self.login_ui.btnClear.clicked.connect(self.loginScr.close)
 
     def show_home_screen(self):
         """
         Display the home page.
-        Currently the code will fail due to homeScr.py line 394, 
-        (if you temporarily remove that line from homeScr.py the code will work)
+        This function uses multiple signals as part of event-driven programming. These signals trigger other functions that
+        allow the user to perform CRUD operations on the database
         """
         # Once we are logged in we can show the Home Screen
         self.homeScr = QWidget() 
@@ -41,7 +43,7 @@ class MainWindow(QMainWindow):
         self.home_ui.setupUi(self.homeScr)
 
         # Setting up Stock Categories tab and assigning slots to signals
-        self.home_ui.SC_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.home_ui.SC_table.setEditTriggers(QAbstractItemView.NoEditTriggers) # Stops the user from double-clicking to edit the table
         self.home_ui.SC_table.cellClicked.connect(self.SCtableSelect)
         self.home_ui.SC_plusBtn.clicked.connect(self.SCaddMode)
 
@@ -49,24 +51,29 @@ class MainWindow(QMainWindow):
         self.home_ui.SC_deleteBtn.setHidden(True)
         self.home_ui.SC_updateBtn.setHidden(True)
 
+        # Assigning functions we created that handle CRUD operations to signals
         self.home_ui.SC_deleteBtn.clicked.connect(self.SCdeleteCategory)
         self.home_ui.SC_updateBtn.clicked.connect(self.SCupdateCategory)
         self.home_ui.SC_addBtn.clicked.connect(self.SCaddCategory)
 
+        # Allowing the user to exit the program through the 'Cancel' button
         self.home_ui.SC_cancelBtn.clicked.connect(self.homeScr.close)
 
         # Setting up Stock Items tab and assigning slots to signals
-        self.home_ui.SI_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.home_ui.SI_table.setEditTriggers(QAbstractItemView.NoEditTriggers) # Stops the user from double-clicking to edit the table
         self.home_ui.SI_table.cellClicked.connect(self.SItableSelect)
         self.home_ui.SI_plusBtn1.clicked.connect(self.SIaddMode)
+
         self.home_ui.SI_addBtn1.setHidden(True)
         self.home_ui.SI_deleteBtn1.setHidden(True)
         self.home_ui.SI_updateBtn1.setHidden(True)
 
+        # Assigning functions we created that handle CRUD operations to signals
         self.home_ui.SI_deleteBtn1.clicked.connect(self.SIdeleteItem)
         self.home_ui.SI_updateBtn1.clicked.connect(self.SIupdateItem)
         self.home_ui.SI_addBtn1.clicked.connect(self.SIaddItem)
 
+        # Allowing the user to exit the program through the 'Cancel' button
         self.home_ui.SI_cancelBtn1.clicked.connect(self.homeScr.close)
 
         # Loading our data unto the Stock Category and Item tables
@@ -76,6 +83,10 @@ class MainWindow(QMainWindow):
         self.homeScr.show()
     
     def SCaddMode(self):
+        '''
+        Stock Categories:
+        Setting up the environment for the 'Add' mode where the user can create records for the stock categories
+        '''
         # Changing mode by hiding specific buttons
         self.home_ui.SC_addBtn.setHidden(False)
         self.home_ui.SC_deleteBtn.setHidden(True)
@@ -87,6 +98,11 @@ class MainWindow(QMainWindow):
         self.home_ui.SC_spinBox.setValue(0)
     
     def SCtableSelect(self,row):
+        '''
+        Stock Categories:
+        Setting up the environment for the 'Edit' mode and populating the edit boxes (and other input methods in the screen)
+        with the information from the record that was selected by the user clicking on it 
+        '''
         # Changing mode by hiding specific buttons
         self.home_ui.SC_addBtn.setHidden(True)
         self.home_ui.SC_deleteBtn.setHidden(False)
@@ -103,12 +119,21 @@ class MainWindow(QMainWindow):
         self.home_ui.SC_spinBox.setValue(int(displayOrder))
 
     def SCdeleteCategory(self):
+        '''
+        Stock Categories:
+        Calling the deleteStockCategory function from our SQL connectivity layer to delete the record selected by the user
+        '''
         id = self.home_ui.SC_IdEbox.toPlainText()
         dataRetrieveDB.deleteStockCategory(id)
         self.load_stock_categories() # Refreshing values in the table to ensure parity with db
         self.load_stock_items() 
 
     def SCupdateCategory(self):
+        '''
+        Stock Categories:
+        Calling the updateStockCategory function from our SQL connectivity layer to update the record selected by the user with the information changed on the 
+        edit boxes or spin box
+        '''
         id = self.home_ui.SC_IdEbox.toPlainText()
         category = self.home_ui.SC_stockCatEbox.toPlainText()
         displayOrder = self.home_ui.SC_spinBox.value()
@@ -117,14 +142,25 @@ class MainWindow(QMainWindow):
         self.load_stock_items()
 
     def SCaddCategory(self):
+        '''
+        Stock Categories:
+        Calling the addStockCategory function from our SQL connectivity layer to create a record with the information entered on the 
+        edit boxes or spin box by the user (the id is automatically generated for data integrity)
+        '''
+        # Declaring variables with data input by the user in the boxes
         category = self.home_ui.SC_stockCatEbox.toPlainText()
         displayOrder = self.home_ui.SC_spinBox.value()
         dataRetrieveDB.addStockCategory(category, displayOrder)
+        
         self.load_stock_categories() # Refreshing values in the table to ensure parity with db
         self.SCaddMode()
-        self.load_stock_items()
+        self.load_stock_items() # Refreshing the data in the comboBox for Categories (in the StockItems tab)
 
     def SIaddMode(self):
+        '''
+        Stock Items:
+        Setting up the environment for the 'Add' mode where the user can create records for the stock categories
+        '''
         # Changing mode by hiding specific buttons
         self.home_ui.SI_addBtn1.setHidden(False)
         self.home_ui.SI_deleteBtn1.setHidden(True)
@@ -140,6 +176,11 @@ class MainWindow(QMainWindow):
         self.home_ui.SI_addInfoEbox.setPlainText("")
 
     def SItableSelect(self,row):
+        '''
+        Stock Items:
+        Setting up the environment for the 'Edit' mode and populating the edit boxes (and other input methods in the screen)
+        with the information from the record that was selected by the user clicking on it 
+        '''
         # Changing mode by hiding specific buttons
         self.home_ui.SI_addBtn1.setHidden(True)
         self.home_ui.SI_deleteBtn1.setHidden(False)
@@ -160,45 +201,59 @@ class MainWindow(QMainWindow):
         self.home_ui.SI_itemUnitEbox.setPlainText(item_unit)
         self.home_ui.SI_itemPriceEbox.setPlainText(item_price)
         self.home_ui.SI_categoryCbox.setCurrentText(category)
-        self.home_ui.SI_availCbox.setCurrentText( "Available" if availability == 'Y' else 'Not Available') # how to edit Cbox
+        self.home_ui.SI_availCbox.setCurrentText( "Available" if availability == 'Y' else 'Not Available') # the data is stored raw as 'Y' and 'N'
         self.home_ui.SI_addInfoEbox.setPlainText(addtl_info)
 
     def SIdeleteItem(self):
+        '''
+        Stock Items:
+        Calling the deleteStockCategory function from our SQL connectivity layer to delete the record selected by the user
+        '''
         id = self.home_ui.SI_idEbox.toPlainText()
         dataRetrieveDB.deleteStockItem(id)
 
     def SIupdateItem(self):
+        '''
+        Stock Items:
+        Calling the updateStockCategory function from our SQL connectivity layer to update the record selected by the user with the information changed on the 
+        edit boxes or combo boxes
+        '''
         id = self.home_ui.SI_idEbox.toPlainText()
         item_name = self.home_ui.SI_itemNameEbox.toPlainText()
         item_unit = self.home_ui.SI_itemUnitEbox.toPlainText()
         item_price = self.home_ui.SI_itemPriceEbox.toPlainText()
-        categoryid = dataRetrieveDB.getCategoryId(self.home_ui.SI_categoryCbox.currentText())
+        categoryid = dataRetrieveDB.getCategoryId(self.home_ui.SI_categoryCbox.currentText()) # Due to manipulation we have to get the id from the category name
         availability = 'Y' if self.home_ui.SI_availCbox.currentText() == 'Available' else 'N'
         addtl_info = self.home_ui.SI_addInfoEbox.toPlainText()
 
-        # Category as ID and not name 
+        # Using SQL connectivity layer function to update the record
         dataRetrieveDB.updateStockItem(id, item_name, item_unit, item_price, categoryid, availability, addtl_info)
 
     def SIaddItem(self):
+        '''
+        Stock Items:
+        Calling the addStockCategory function from our SQL connectivity layer to create a record with the information entered on the 
+        edit boxes or spin box by the user (the id is automatically generated for data integrity)
+        '''
         id = self.home_ui.SI_idEbox.toPlainText()
         item_name = self.home_ui.SI_itemNameEbox.toPlainText()
         item_unit = self.home_ui.SI_itemUnitEbox.toPlainText()
         item_price = self.home_ui.SI_itemPriceEbox.toPlainText()
-        categoryid = dataRetrieveDB.getCategoryId(self.home_ui.SI_categoryCbox.currentText())
+        categoryid = dataRetrieveDB.getCategoryId(self.home_ui.SI_categoryCbox.currentText()) # Due to manipulation we have to get the id from the category name
         availability = 'Y' if self.home_ui.SI_availCbox.currentText() == 'Available' else 'N'
         addtl_info = self.home_ui.SI_addInfoEbox.toPlainText()
 
-        # Category as ID and not name 
+        # Using SQL connectivity layer function to add the record
         dataRetrieveDB.addStockItem(id, item_name, item_unit, item_price, categoryid, availability, addtl_info)
     
     def login(self):
         """
-        User authenticates against the database. Uses the functions from the SQL Connection Layer.
+        User authenticates against the database. Uses the functions from the SQL Connection Layer to validate the credentials.
         """
-
         # Using the SQL Connectivity Layer functions to check if the user exists in the database 
         # and check whether their password is correct.
         if dataRetrieveDB.checkUser(self.login_ui.editboxUsr.text()) and dataRetrieveDB.checkPassword(self.login_ui.editboxUsr.text(), self.login_ui.editboxPass.text()):
+            # Displays message response to user to know they are now logged in
             message = QMessageBox()
             message.setText("Logged in.")
             message.setIcon(QMessageBox.Information)
@@ -208,6 +263,7 @@ class MainWindow(QMainWindow):
             self.loginScr.hide()
             self.show_home_screen()
         else:
+            # Displays message response to user to know that the credentials were invalid
             message = QMessageBox()
             message.setText("Please check your username and password.")
             message.setIcon(QMessageBox.Critical)
@@ -216,16 +272,20 @@ class MainWindow(QMainWindow):
             message.exec_()
 
     def load_stock_items(self):
-        # Stock items
+        '''
+        Gets the records for StockItems in the database and puts the values into the table to display to the user.
+        '''
+        # Getting a list of our stock items
         stock_items = dataRetrieveDB.getStockItems()
         
         # Setup drop down to select category
         self.home_ui.SI_categoryCbox.clear()
-        self.home_ui.SI_categoryCbox.addItem("Category Select") # Default
+        self.home_ui.SI_categoryCbox.addItem("Category Select") # Adding the default value
         stock_categories = dataRetrieveDB.getStockCategories() 
         for i in stock_categories:
             self.home_ui.SI_categoryCbox.addItem(i["stockcategory"])
             
+        # Loading Stock item data into the Stock Items table
         row = 0
         self.home_ui.SI_table.setRowCount(len(stock_items))
         for item in stock_items: # Inserting them into our QTableWidget with a grid-like structure
@@ -242,6 +302,9 @@ class MainWindow(QMainWindow):
             row += 1
     
     def load_stock_categories(self):
+        '''
+        Gets the records for StockCategory in the database and puts the values into the table to display to the user.
+        '''
         # Stock categories
         stock_categories = dataRetrieveDB.getStockCategories()
         row = 0
@@ -252,10 +315,11 @@ class MainWindow(QMainWindow):
             self.home_ui.SC_table.setItem(row, 2,  QTableWidgetItem(str(item['displayorder'])))
             row += 1
 
-def main():
+if __name__ == "__main__":
+    '''
+    Initialises the application with a main call to our PyQt5Widget
+    '''
     dataRetrieveDB.connectToDB() # Connect to the database before user attempts to log in
     app = QApplication([])
     window = MainWindow()
     app.exec()
-
-main()
